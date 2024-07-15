4701255a38104a2c12309c0859f3bc805673f253
KripaMishra <kripa5661@gmail.com>
2024-07-15 17:08:27 +0000

    semantic chunking

---
+++ components/semantic_chunking.py
@@ -0,0 +1,59 @@
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+from langchain_experimental.text_splitter import SemanticChunker
+from sentence_transformers import SentenceTransformer
+from tqdm import tqdm
+import torch
+
+class CustomEmbeddings:
+    def __init__(self, model_name, device):
+        self.model = SentenceTransformer(model_name, device=device)
+
+    def embed_documents(self, texts):
+        return self.model.encode(texts, show_progress_bar=False)
+
+def semantic_chunk_to_documents(file_path, chunk_size=1000, chunk_overlap=200):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using device: {device}")
+
+    model_name = 'all-MiniLM-L6-v2'
+
+    # Create custom embedding object
+    embeddings = CustomEmbeddings(model_name, device)
+
+    # Initialize the semantic chunker
+    text_splitter = SemanticChunker(embeddings=embeddings)
+
+    # Read the content of the file
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    # Split the text into chunks
+    print("Semantically chunking the text...")
+    chunks = text_splitter.split_text(text)
+
+    # Create the documents list with IDs
+    print("Creating document objects...")
+    documents = []
+
+    # Process chunks
+    for i, chunk in tqdm(enumerate(chunks), total=len(chunks), desc="Processing chunks"):
+        documents.append({"id": i + 1, "content": chunk})
+
+    return documents
+
+import json
+
+def export_to_json(documents, output_file):
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(documents, f, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    # Usage example
+    file_path = "/home/ubuntu/project/steps/formatted_data.txt"
+    chunk_data_path="/home/ubuntu/project/Steps/chunk_data.json"
+    documents = semantic_chunk_to_documents(file_path)
+    export_to_json(documents,chunk_data_path)
+
